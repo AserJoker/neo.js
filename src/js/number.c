1a4ea06d16d0f121614166c1b993be63172dd45c
aserjoker <aserjoker@outlook.com>
2025-06-16 20:53:29 +0800

    #feat #number

--- src/js/number.c
+++ src/js/number.c
@@ -47,13 +47,54 @@ static neo_js_variable_t neo_js_number_to_number(neo_js_context_t ctx,
                                                  neo_js_variable_t self) {
   return self;
 }
+static neo_js_variable_t neo_js_number_to_primitive(neo_js_context_t ctx,
+                                                    neo_js_variable_t self) {
+  return self;
+}
+static neo_js_variable_t neo_js_number_to_object(neo_js_context_t ctx,
+                                                 neo_js_variable_t self) {
+  return neo_js_context_construct(
+      ctx, neo_js_context_get_number_constructor(ctx), 1, &self);
+}
+static neo_js_variable_t neo_js_number_get_field(neo_js_context_t ctx,
+                                                 neo_js_variable_t self,
+                                                 neo_js_variable_t field) {
+  return neo_js_context_get_field(ctx, neo_js_number_to_object(ctx, self),
+                                  field);
+}
+static neo_js_variable_t neo_js_number_set_field(neo_js_context_t ctx,
+                                                 neo_js_variable_t self,
+                                                 neo_js_variable_t field,
+                                                 neo_js_variable_t value) {
+  return neo_js_context_set_field(ctx, neo_js_number_to_object(ctx, self),
+                                  field, value);
+}
+
+static neo_js_variable_t neo_js_number_del_field(neo_js_context_t ctx,
+                                                 neo_js_variable_t self,
+                                                 neo_js_variable_t field) {
+  return neo_js_context_del_field(ctx, neo_js_number_to_object(ctx, self),
+                                  field);
+}
+static bool neo_js_number_is_equal(neo_js_context_t ctx, neo_js_variable_t self,
+                                   neo_js_variable_t another) {
+  neo_js_value_t val1 = neo_js_variable_get_value(self);
+  neo_js_value_t val2 = neo_js_variable_get_value(another);
+  neo_js_number_t num1 = neo_js_value_to_number(val1);
+  neo_js_number_t num2 = neo_js_value_to_number(val2);
+  if (isnan(num1->number) || isnan(num2->number)) {
+    return false;
+  }
+  return num1->number == num2->number;
+}
 
 neo_js_type_t neo_get_js_number_type() {
   static struct _neo_js_type_t type = {
-      neo_js_number_typeof,
-      neo_js_number_to_string,
-      neo_js_number_to_boolean,
-      neo_js_number_to_number,
+      neo_js_number_typeof,       neo_js_number_to_string,
+      neo_js_number_to_boolean,   neo_js_number_to_number,
+      neo_js_number_to_primitive, neo_js_number_to_object,
+      neo_js_number_get_field,    neo_js_number_set_field,
+      neo_js_number_del_field,    neo_js_number_is_equal,
   };
   return &type;
 }
